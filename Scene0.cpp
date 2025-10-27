c9126c4b3925910b75a9fa0457b2911b445a35d8
AnanthC099 <ananthchandrasekharan099@gmail.com>
2025-10-27 12:55:58 +0530

    Ensure GLM experimental extensions are enabled

--- Scene0.cpp
+++ Scene0.cpp
@@ -1,5 +1,14 @@
 #include <string.h>
 
+#ifndef GLM_FORCE_RADIANS
+#define GLM_FORCE_RADIANS
+#endif
+#ifndef GLM_FORCE_DEPTH_ZERO_TO_ONE
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#endif
+#ifndef GLM_ENABLE_EXPERIMENTAL
+#define GLM_ENABLE_EXPERIMENTAL
+#endif
 #include "glm/gtc/matrix_transform.hpp"
 
 #include "SceneSwitcher.h"
--- Scene1.cpp
+++ Scene1.cpp
@@ -4,6 +4,15 @@
 #include <stdlib.h>
 #include <string.h>
 
+#ifndef GLM_FORCE_RADIANS
+#define GLM_FORCE_RADIANS
+#endif
+#ifndef GLM_FORCE_DEPTH_ZERO_TO_ONE
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#endif
+#ifndef GLM_ENABLE_EXPERIMENTAL
+#define GLM_ENABLE_EXPERIMENTAL
+#endif
 #include "glm/gtc/constants.hpp"
 #include "glm/gtx/quaternion.hpp"
 
