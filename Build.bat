0ff38f2cb5049366f1ed45006dcd059651889e0c
AnanthC099 <ananthchandrasekharan099@gmail.com>
2025-10-27 12:38:12 +0530

    Update build script for scene modules

--- Build.bat
+++ Build.bat
@@ -14,11 +14,11 @@ glslangValidator.exe -V -H -o Shader_Scene2.vert.spv Shader_Scene2.vert
 
 glslangValidator.exe -V -H -o Shader_Scene2.frag.spv Shader_Scene2.frag
 
-cl.exe /c /EHsc /I C:\VulkanSDK\Anjaneya\Include SceneSwitcher.cpp 
+cl.exe /c /EHsc /I C:\VulkanSDK\Anjaneya\Include SceneSwitcher.cpp Scene0.cpp Scene1.cpp Scene2.cpp
 
 rc.exe Scene0.rc
 
-link.exe  SceneSwitcher.obj Scene0.res /LIBPATH:C:\VulkanSDK\Anjaneya\Lib /SUBSYSTEM:WINDOWS
+link.exe  SceneSwitcher.obj Scene0.obj Scene1.obj Scene2.obj Scene0.res /LIBPATH:C:\VulkanSDK\Anjaneya\Lib /SUBSYSTEM:WINDOWS
 
 SceneSwitcher.exe
 
